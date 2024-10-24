a1509b1846450a7f04eaf5f88b108b7f3f5a75b0
Marie <m.aschmann@icloud.com>
2024-10-24 15:10:10 +0200

    Add shadowJar task to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jetbrains.kotlin.ir.backend.js.compile
+
 plugins {
     kotlin("jvm") version "2.0.20"
     kotlin("kapt") version "2.0.21"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "social.marylieh.secureproxy"
@@ -14,6 +17,8 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     compileOnly("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
     annotationProcessor("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
     kapt("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
@@ -21,4 +26,8 @@ dependencies {
 
 kotlin {
     jvmToolchain(21)
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
 }
